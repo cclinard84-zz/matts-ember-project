d6118be91bc78c1635ec53344c1dc9d20bf0e81b
matt <Matt.Clinard@kub.org>
2017-05-22 14:11:53 -0400

    feat: Added about, contact, and rentals pages

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,12 @@
+import { test } from 'qunit';
+import moduleForAcceptance from 'matts-ember-project/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('visiting /', function(assert) {
+  visit('/');
+
+  andThen(function() {
+    assert.equal(currentURL(), '/');
+  });
+});
