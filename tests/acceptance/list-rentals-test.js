6671272538b690d855f7935702a6687e12734347
Matt <cclinard84@gmail.com>
2017-05-22 16:03:41 -0400

    Matt build branch (#1)
    * feat: Added about, contact, and rentals pages
    
    * feat: Added link testing
    
    * feat: Added CSS to style the Super Rental Website
    
    * feat: Added sizeable pictures and tests to make sure that feature works properly
    
    * feat: Added API for rental data

---
+++ tests/acceptance/list-rentals-test.js
@@ -0,0 +1,45 @@
+import {
+  test
+} from 'qunit';
+import moduleForAcceptance from 'matts-ember-project/tests/helpers/module-for-acceptance';
+
+moduleForAcceptance('Acceptance | list rentals');
+
+test('should show rentals as the home page', function(assert){
+  visit('/');
+  andThen(function() {
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
+
+test('should link to information about the company', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function() {
+    assert.equal(currentURL(), '/about', 'should navigate to About');
+  });
+});
+
+test('should link to contact information', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should navigate to Contact');
+  });
+});
+
+test('should navigate from about to rentals', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  click('a:contains("Super Rentals")');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should navigate to rentals');
+  });
+});
+
+test('should list the available rentals', function(assert){
+  visit('/');
+  andThen(function(){
+    assert.equal(find('.listing').length, 3, 'should see 3 listings');
+  });
+});
---
+++ tests/integration/helpers/rental-property-type-test.js
@@ -0,0 +1,7 @@
+
+import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
+
+moduleForComponent('rental-property-type', 'helper:rental-property-type', {
+  integration: true
+});
