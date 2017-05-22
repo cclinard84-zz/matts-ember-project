87bab89859a78939f6467012411c67c1df02be18
matt <Matt.Clinard@kub.org>
2017-05-22 14:25:54 -0400

    feat: Added link testing

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -3,10 +3,34 @@ import moduleForAcceptance from 'matts-ember-project/tests/helpers/module-for-ac
 
 moduleForAcceptance('Acceptance | list rentals');
 
-test('visiting /', function(assert) {
+test('should show rentals as the home page', function(assert){
   visit('/');
-
   andThen(function() {
-    assert.equal(currentURL(), '/');
+    assert.equal(currentURL(), '/rentals', 'should redirect automatically');
+  });
+});
+
+test('should link to information about the company', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  andThen(function() {
+    assert.equal(currentURL(), '/about', 'should navigate to About');
+  });
+});
+
+test('should link to contact information', function(assert){
+  visit('/');
+  click('a:contains("Contact")');
+  andThen(function(){
+    assert.equal(currentURL(), '/contact', 'should navigate to Contact');
+  });
+});
+
+test('should navigate from about to rentals', function(assert){
+  visit('/');
+  click('a:contains("About")');
+  click('a:contains("Super Rentals")');
+  andThen(function(){
+    assert.equal(currentURL(), '/rentals', 'should navigate to rentals');
   });
 });
